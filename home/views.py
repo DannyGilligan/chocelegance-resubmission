a7895d6cfb118bcd9db93e953ac9bb12739ffb6e
DannyGilligan <gilligan.danny@gmail.com>
2024-05-24 00:26:53 +0000

    Add url paths for home app to urls.py

--- home/views.py
+++ home/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    """
+    This view will return the index page
+    """
+    return render(request, 'home/index.html')

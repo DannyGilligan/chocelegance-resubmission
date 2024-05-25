7d9fd0a5f2df8d0b0746885591d0584b620862f2
DannyGilligan <gilligan.danny@gmail.com>
2024-05-25 23:08:36 +0000

    Add allchocolates view to chocolates app

--- chocolates/admin.py
+++ chocolates/admin.py
@@ -4,6 +4,10 @@ from .models import Chocolate, ChocolateCategory, DietaryType
 # Register your models here.
 
 class ChocolateAdmin(admin.ModelAdmin):
+    """
+    Controls the fields displayed to user in the admin
+    panel list when viewing 'Chocolates'
+    """
     list_display = (
         'choc_name',
         'choc_dietary_type',
@@ -17,13 +21,23 @@ class ChocolateAdmin(admin.ModelAdmin):
 
 
 class ChocolateCategoryAdmin(admin.ModelAdmin):
+    """
+    Controls the fields displayed to user in the admin
+    panel list when viewing 'Chocolate Categories'
+    """
     list_display = (
+        'choc_category',
         'choc_category_desc',
     )
 
 
 class DietaryTypeAdmin(admin.ModelAdmin):
+    """
+    Controls the fields displayed to user in the admin
+    panel list when viewing 'Dietary Types'
+    """
     list_display = (
+        'dietary_type',
         'dietary_description',
     )
 
--- chocolates/views.py
+++ chocolates/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Chocolate
 
 # Create your views here.
+
+def AllChocolates(request):
+    """
+    This view will show all chocolate, along with sorting
+    and search queries
+    """
+
+    chocolates = Chocolate.objects.all()
+
+    context = {
+        'chocolates': chocolates,
+    }
+    return render(request, 'chocolates/chocolates.html', context)

43cf874e4c99ae33cda80e558635025fd4b79f0f
DannyGilligan <gilligan.danny@gmail.com>
2024-05-25 22:59:24 +0000

    Update chocolate model rating and price fields

--- chocolates/admin.py
+++ chocolates/admin.py
@@ -2,6 +2,36 @@ from django.contrib import admin
 from .models import Chocolate, ChocolateCategory, DietaryType
 
 # Register your models here.
-admin.site.register(Chocolate)
-admin.site.register(ChocolateCategory)
-admin.site.register(DietaryType)
+
+class ChocolateAdmin(admin.ModelAdmin):
+    list_display = (
+        'choc_name',
+        'choc_dietary_type',
+        'choc_category',
+        'choc_price',
+        
+        'choc_image',
+    )
+
+    ordering = ('choc_name',)
+
+
+class ChocolateCategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'choc_category_desc',
+    )
+
+
+class DietaryTypeAdmin(admin.ModelAdmin):
+    list_display = (
+        'dietary_description',
+    )
+
+
+
+
+admin.site.register(Chocolate, ChocolateAdmin)
+admin.site.register(ChocolateCategory, ChocolateCategoryAdmin)
+admin.site.register(DietaryType, DietaryTypeAdmin)
+
+

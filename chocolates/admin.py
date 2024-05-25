07ddfa962b64132441b4c569679f5511021f820a
DannyGilligan <gilligan.danny@gmail.com>
2024-05-25 22:22:51 +0000

    Register models in admin.py

--- chocolates/admin.py
+++ chocolates/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Chocolate, ChocolateCategory, DietaryType
 
 # Register your models here.
+admin.site.register(Chocolate)
+admin.site.register(ChocolateCategory)
+admin.site.register(DietaryType)

055a5123ea1157e1d3d62d0ae5a2b5adbe9fa594
DannyGilligan <gilligan.danny@gmail.com>
2024-05-26 01:09:45 +0000

    Add chocolate_detail view to chocolates app

--- chocolates/views.py
+++ chocolates/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Chocolate
 
 # Create your views here.
@@ -14,5 +14,20 @@ def all_chocolates(request):
     context = {
         'chocolates': chocolates,
     }
-    
+
     return render(request, 'chocolates/chocolates.html', context)
+
+
+def chocolate_detail(request, product_id):
+    """
+    This view will render the details for an individual chocolate 
+    based on the chocolate id
+    """
+
+    chocolate = get_object_or_404(Chocolate, pk=product_id)
+
+    context = {
+        'chocolate': chocolates,
+    }
+    
+    return render(request, 'chocolates/chocolate_detail.html', context)

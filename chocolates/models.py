4c6a43c49981ab2eee5a26f9a7783aeaccc75ea2
DannyGilligan <gilligan.danny@gmail.com>
2024-05-26 18:22:06 +0000

    Update chocolates model in chocolates app after database reset

--- chocolates/models.py
+++ chocolates/models.py
@@ -36,8 +36,6 @@ class DietaryType(models.Model):
         verbose_name_plural = "Dietary Types"
 
 
-
-
 # Custom Model 2
 class ChocolateCategory(models.Model):
     """
@@ -45,7 +43,6 @@ class ChocolateCategory(models.Model):
     chocolate categories offered by Chocelegance and their description
     (e.g, Dark Chocolate, Milk Chocolate and White Chocolate etc)
     """
-    
     choc_category_name = models.CharField(max_length=200)
     choc_friendly_name = models.CharField(
         max_length=200, null=True, blank=True
@@ -71,3 +68,44 @@ class ChocolateCategory(models.Model):
         Model name to be displayed in admin panel
         """
         verbose_name_plural = "Chocolate Categories"
+
+
+# Custom Model 3
+class Chocolate(models.Model):
+    """
+    The Chocolate Model will hold the details for all chocolates sold
+    by Chocelegance. This includes all the necessary details that
+    customers expect, such as price, description, image, ingredients,
+    macro-nutrional values etc.
+    """
+    choc_dietary_type = models.ForeignKey(
+        'DietaryType', null=True, blank=True, on_delete=models.SET_NULL
+    )
+    choc_category_display = models.ForeignKey(
+        'ChocolateCategory', null=True, blank=True, on_delete=models.SET_NULL
+    )
+
+    choc_name = models.CharField(max_length=200)
+    choc_friendly_name = models.CharField(
+        max_length=200, null=True, blank=True
+    )
+    choc_description = models.TextField()
+    choc_rating = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True
+    )
+    choc_image = models.ImageField(null=True, blank=True)
+    choc_image_url = models.URLField(max_length=2000, null=True, blank=True)
+    choc_price = models.DecimalField(max_digits=5, decimal_places=2)
+    choc_pieces = models.IntegerField(null=True, blank=True)
+    choc_ingredients = models.TextField()
+    choc_fat = models.FloatField()
+    choc_protein = models.FloatField()
+    choc_carbs = models.FloatField()
+    choc_sugar = models.FloatField()
+
+    def __str__(self):
+        """
+        This function returns the model name for easier readibility in
+        the admin panel
+        """
+        return self.choc_name

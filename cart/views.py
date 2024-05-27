5a524b55538f3eb8d29ec49dcbcc76d3c3ea184f
DannyGilligan <gilligan.danny@gmail.com>
2024-05-27 14:09:05 +0000

    Add view_cart view to cart app

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def view_cart(request):
+    """
+    This view will return the shopping cart contents page
+    """
+    return render(request, 'cart/cart.html')

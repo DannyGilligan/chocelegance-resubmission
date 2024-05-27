389cc2a8d74d86bfcde3a5742dfc5e7359ed4c4f
DannyGilligan <gilligan.danny@gmail.com>
2024-05-27 16:51:33 +0000

    Add cart_contents to templates variable in settings.py

---
+++ cart/contexts.py
@@ -0,0 +1,14 @@
+
+
+def cart_contents(request):
+    """
+    This function returns a dictionary and stores it in the
+    context variable, this context can then be accessed by
+    all templates across the entire site
+    """
+
+    context = {
+        
+    }
+
+    return context

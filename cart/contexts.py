9ec3df8c0258fd6e745dac0fa5e18ac5a55ccd15
DannyGilligan <gilligan.danny@gmail.com>
2024-05-27 17:01:57 +0000

    Add logic to contexts.py in cart app

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,14 +1,35 @@
-
+from decimal import Decimal
+from django.conf import settings
 
 def cart_contents(request):
     """
     This function returns a dictionary and stores it in the
     context variable, this context can then be accessed by
-    all templates across the entire site
+    all templates across the entire site by adding it to
+    the templates variable in settings.py
     """
 
+    cart_items = []
+    total = 0
+    product_count = 0
+
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    else:
+        delivery = 0
+        free_delivery_delta = 0
+
+    grand_total = delivery + total
+
     context = {
-        
+        'cart_items': cart_items,
+        'total': total,
+        'product_count': product_count,
+        'delivery': delivery,
+        'free_delivery_delta': free_delivery_delta,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+        'grand_total': grand_total,
     }
 
     return context

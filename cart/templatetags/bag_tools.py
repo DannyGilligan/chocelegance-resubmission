b327dbbf503c24b704c9a1bb7e47b8af29ebd95d
DannyGilligan <gilligan.danny@gmail.com>
2024-05-28 00:28:17 +0000

    Add bag_tools.py to templatetags folder in cart app

---
+++ cart/templatetags/bag_tools.py
@@ -0,0 +1,12 @@
+from django import template
+
+
+register = template.Library()
+
+@register.filter(name='calc_subtotal')
+def calc_subtotal(price, quantity):
+    """
+    This function will calculate the
+    subtotal amount to be shown on the checkout page
+    """
+    return price * quantity

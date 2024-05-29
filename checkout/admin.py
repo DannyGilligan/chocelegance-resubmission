6b2ffad715fef555d08f2ea7685bd57bbb61a66f
DannyGilligan <gilligan.danny@gmail.com>
2024-05-29 22:49:49 +0000

    Register the order and orderadmin models in the admin panel

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,11 +1,42 @@
 from django.contrib import admin
 from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    """
+    Registers the OrderLineItem model in the admin panel
+    and makes the lineitem_total field readonly as this
+    field is calculated by the model itself. Allows us
+    to edit line items directly from the order.
+    """
+
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
 
 class OrderAdmin(admin.ModelAdmin):
-    readonly_fields = ('order_number', 'date', 
+    """
+    Registers the Order model in the admin and makes
+    the relevant calculated fields readonly.
+    """
+
+    inlines = (OrderLineItemAdminInline,)
+
+    readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
                        'grand_total',)
 
+    fields = ('order_number', 'date', 'full_name',
+              'email', 'phone_number', 'country',
+              'postcode', 'town_or_city', 'street_address1',
+              'street_address2', 'county', 'delivery_cost',
+              'order_total', 'grand_total',)
 
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_total', 'delivery_cost',
+                    'grand_total',)
+
+    ordering = ('-date',)  # Most recent first
+
+
+admin.site.register(Order, OrderAdmin)

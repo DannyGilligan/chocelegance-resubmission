e99b731e73470d21f998a83509be7817bed7d3fb
DannyGilligan <gilligan.danny@gmail.com>
2024-05-29 23:00:21 +0000

    Update apps.py with ready method in checkout app

--- checkout/apps.py
+++ checkout/apps.py
@@ -4,3 +4,12 @@ from django.apps import AppConfig
 class CheckoutConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'checkout'
+
+    def ready(self):
+        """
+        When line item is saved or delted,
+        this will trigger the updating of
+        order totals automatically by calling
+        the relevent model method.
+        """
+        import checkout.signals

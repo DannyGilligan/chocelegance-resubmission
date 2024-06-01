2bab40fe8baa11382c7e3f6b4a437e6776ebadec
DannyGilligan <gilligan.danny@gmail.com>
2024-06-01 22:36:01 +0000

    Add style rule for card error message

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -7,7 +7,6 @@
 */
 
 
-
 var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
 var client_secret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripe_public_key);
@@ -33,3 +32,22 @@ var style = {
 
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
+
+
+// Handle realtime validation errors on the card element
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <div id='card-error-container'>
+                <span class="icon" role="alert">
+                    <i class="fas fa-times"></i>
+                </span>
+                <span>${event.error.message}</span>
+            </div>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});

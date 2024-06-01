2aca67b916631bfe60b9a08055a8de64b43d4fab
DannyGilligan <gilligan.danny@gmail.com>
2024-06-01 22:22:58 +0000

    Fix card number input placeholder opacity styling

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -13,19 +13,23 @@ var client_secret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripe_public_key);
 var elements = stripe.elements();
 var style = {
-    base: {
+    base: { 
         color: '#000',
         fontFamily: '"Urbanist", "Helvetica Neue", Helvetica, sans-serif',
         fontSmoothing: 'antialiased',
         fontSize: '16px',
         '::placeholder': {
-            color: '#aab7c4'
-        }
+            // The value 60 has been appended to the color code '#551b8c' below to change the opacity
+            color: '#551b8c60',
+            fontFamily: '"Urbanist, "Helvetica Neue", Helvetica, sans-serif',
+        },
     },
     invalid: {
         color: '#eb1c26',
         iconColor: '#dc3545'
     }
 };
+
+
 var card = elements.create('card', {style: style});
 card.mount('#card-element');

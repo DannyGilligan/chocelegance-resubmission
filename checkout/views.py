f9e8fc0542881cfce716d95f75bee3e68da1caf5
DannyGilligan <gilligan.danny@gmail.com>
2024-06-01 20:23:29 +0000

    Add stripe styling to checkout.css

--- checkout/views.py
+++ checkout/views.py
@@ -25,7 +25,7 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': '',
+        'stripe_public_key': 'fake_key',
         'client_secret': 'test client secret',
     }
 

faf23d11dcbb4bb648c80281d5eeb992a6fdbcf9
DannyGilligan <gilligan.danny@gmail.com>
2024-06-01 23:05:03 +0000

    Add stripe logic to checkout view in checkout app

--- checkout/views.py
+++ checkout/views.py
@@ -13,9 +13,14 @@ import stripe
 def checkout(request):
     """
     This view will allow the user to complete the checkout
-    process and purchase the items in the cart
+    process and purchase the items in the cart, it also
+    contains the logic to implement Stripe functionality
     """
-    
+
+    # Imports the stripe key values from settings.py
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     # Cart contents are stored in the cart variable
     cart = request.session.get('cart', {})
 
@@ -25,21 +30,36 @@ def checkout(request):
         # Customer is redirected back to the chocolates page
         return redirect(reverse('chocolates'))
 
-
     # Gets the cart values from the cart_contents view
     current_cart = cart_contents(request)
 
     # Gets the total by accessing the grand total key
     total = current_cart['grand_total']
 
+    # Sets the secret key in Stripe
+    stripe.api_key = stripe_secret_key
+
     # Rounds the number to two decimal places for processing payment
     stripe_total = round(total * 100)
+
+    # Creates the payment intent
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
+
+    # Error message will be displayed if the public key is missing
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. Did \
+            you forget to set it as an environment variable?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'fake_key',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)

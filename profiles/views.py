4cb490e598ba713d7e64fdd7db5b28083971d85d
DannyGilligan <gilligan.danny@gmail.com>
2024-06-03 23:48:18 +0000

    Update post handler for profile view in profiles app

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
@@ -12,13 +13,20 @@ def profile(request):
     """ Displays a template rendering the user's profile """
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your profile has been updated!')
+
     form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
     context = {
         'form': form,
-        'profile': profile,
+        'orders': orders,
+        'on_profile_page': True
     }
 
     return render(request, template, context)

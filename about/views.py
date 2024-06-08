48059cd7fb5e82b98d06124493fa0afab6d640e8
DannyGilligan <gilligan.danny@gmail.com>
2024-06-08 23:52:18 +0000

    Add about view to about app

--- about/views.py
+++ about/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    """
+    This view will return the about page
+    """
+    return render(request, 'about/about.html')

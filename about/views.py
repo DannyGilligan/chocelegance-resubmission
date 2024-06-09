2ef5beaa5c2138434354f5240821dfaa5ddc0a66
DannyGilligan <gilligan.danny@gmail.com>
2024-06-09 16:29:17 +0000

    Add faqs context to about view in about app

--- about/views.py
+++ about/views.py
@@ -7,4 +7,11 @@ def about(request):
     """
     This view will return the about page
     """
+
+    faqs = Faq.objects.all()
+
+    context = {
+        'faqs': faqs,
+    }
+    
     return render(request, 'about/about.html')

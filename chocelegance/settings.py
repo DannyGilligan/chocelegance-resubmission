d132c7b502c1e735f0a7bfd4b07702cce4346bc4
DannyGilligan <gilligan.danny@gmail.com>
2024-06-09 18:39:58 +0000

    Add testimonials to installed apps in settings.py

--- chocelegance/settings.py
+++ chocelegance/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'checkout',
     'profiles',
     'about',
+    'testimonials',
 
     # Other apps
     'crispy_forms',
--- testimonials/models.py
+++ testimonials/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from profiles.models import UserProfile
 
 # Create your models here.
+
+
 
 # Custom model 5
 

792eb888f11fa18d7d99e7499370998148261582
DannyGilligan <gilligan.danny@gmail.com>
2024-06-09 18:41:55 +0000

    Register testimonial model in admin.py

--- testimonials/admin.py
+++ testimonials/admin.py
@@ -1,1 +1,18 @@
+from django.contrib import admin
+from .models import Testimonial
 
+# Register your models here.
+
+class TestimonialAdmin(admin.ModelAdmin):
+    """
+    Registers the Testimonial model in the admin.
+    """
+
+    fields = ('testimonial_user', 'testimonial_user_display', 'testimonial_text', 'testimonial_date', 'testimonial_publish')
+
+
+    list_display = ('testimonial_user_display', 'testimonial_date', 'testimonial_text',)
+
+    ordering = ('-testimonial_date',)  # Most recent first
+
+admin.site.register(Testimonial, TestimonialAdmin)

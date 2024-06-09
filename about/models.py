bbc3a0e430219f0b6f25e86f27a7df0d04f60363
DannyGilligan <gilligan.danny@gmail.com>
2024-06-09 16:21:37 +0000

    Make migrations for faq model

--- about/models.py
+++ about/models.py
@@ -17,9 +17,9 @@ class Faq(models.Model):
         ("No", "Yes"),
     )
 
-    faq_question = models.CharField(max_length=500)
+    faq_question = models.CharField(max_length=500, null=False, blank=False)
     faq_answer = models.TextField()
-    faq_publish = models.CharField(max_length=3, choices=CHOICES)
+    faq_publish = models.CharField(null=False, blank=False, max_length=3, choices=CHOICES)
 
     def __str__(self):
         """

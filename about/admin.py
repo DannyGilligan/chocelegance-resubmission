d5da8eb07820d390ee56fd99e17c6ef322684bd5
DannyGilligan <gilligan.danny@gmail.com>
2024-06-09 16:12:45 +0000

    Register faq model in admin.py in abuot app

--- about/admin.py
+++ about/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+
+class FaqAdmin(admin.ModelAdmin):
+    """
+    Registers the FAQ model in the admin.
+    """
+
+    fields = ('faq_question', 'faq_answer', 'faq_publish')
+
+    list_display = ('faq_question', 'faq_publish',)
+
+    ordering = ('faq_publish',)  # Published FAQs shown first
+
+
+admin.site.register(FaqAdmin)
